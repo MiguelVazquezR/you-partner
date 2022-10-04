9855567af7fdfc45ddaf1e8c89709b06a510e8eb
Angel <angelvazquez470@gmail.com>
2022-10-03 19:07:30 -0500

    modulo de reporte de errores

--- app/Http/Controllers/ErrorReportController.php
+++ app/Http/Controllers/ErrorReportController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ErrorReportModel;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ErrorReportController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $filters = $request->all('search');
+        $errors = ErrorReportModel::all()
+            ->filter($filters)
+            ->with('user')
+            ->latest()
+            ->paginate();
 
-        return Inertia::render('ErrorReport/Index');
+        return Inertia::render('ErrorReport/Index', compact('errors'));
+
     }
 }
